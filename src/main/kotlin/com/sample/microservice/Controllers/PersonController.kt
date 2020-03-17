1aa2453cf2e22ccc030c7c5e961bea287b2ecc2e
dizsart <darogadibia@gmail.com>
2020-03-17 11:24:25 +0100

    added the swaggerconfig filepath to the file structure of the microservice project tree

--- src/main/kotlin/com/sample/microservice/Controllers/PersonController.kt
+++ src/main/kotlin/com/sample/microservice/Controllers/PersonController.kt
@@ -1,16 +1,27 @@
 package com.sample.microservice.Controllers
 
+import com.sample.microservice.Models.Person
 import com.sample.microservice.Repository.PersonRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/persons")
-class PersonController {
+class PersonController (var repository: PersonRepository) {
 
-    @Autowired
-    lateint var repository: PersonRepository
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): Person? = repository.findById(id)
 
+    @GetMapping
+    fun findAll(): List<Person> = repository.findAll()
+
+    @PostMapping
+    fun add(@RequestBody person: Person): Person = repository.save(person)
+
+    @PutMapping
+    fun update(@RequestBody person: Person): Person = repository.update(person)
+
+    @DeleteMapping("/{id}")
+    fun remove(@PathVariable id: Int): Boolean = repository.removeById(id)
 
 }
